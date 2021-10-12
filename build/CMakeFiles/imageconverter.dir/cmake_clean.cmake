f9b056e4fccc915517f43f78b242659538092dfa
Fábio Gonçalves <fabiommgoncalves@gmail.com>
2021-10-12 19:12:42 +0100

    first commit

---
+++ build/CMakeFiles/imageconverter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imageconverter.dir/main.cpp.o"
+  "imageconverter"
+  "imageconverter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imageconverter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main(int argc, char* argv[]) 
+
+/*argumento passados pela linha de comandos
+ 0-caminho do executável
+ 1- --nome
+ 2- "passar nome do ficheiro  "*/
+{
+    setlocale(LC_ALL, "pt-pt");
+    for(int i=0;i<argc;i++)
+    {
+        std::cout<<argv[i]<<std::endl;
+    }
+
+return 0;
+ 
+}
