21c51af5c4bbb660b75ba75ee54e1d6045c9f2ec
Fábio Gonçalves <fabiommgoncalves@gmail.com>
2021-10-16 15:55:34 +0100

    updata sab

--- main.cpp
+++ main.cpp
@@ -5,27 +5,56 @@
 class ArgumentParser
 {
 public:
-    void RegisterFlag(const std::string& flag)
+    void RegisterFlag(const std::string& flag) // metodo register - regista e gurda as flag´s
     {
          if(!flag.empty())
         {
-            m_Flags[flag]=false;
-
+            m_flags[flag]=false;
+ 
         }
         
 
     }
-    bool GetFlag(const std::string &flag)
+    bool GetFlag(const std::string &flag) const
     {
+        if(!flag.empty())// check if flag have something / Can´t is empty
+        {
+            auto flagit = m_flags.find(flag); //function that finds flag inside the dictionary / associate an iterator that will go through dictionary 
+            if(flagit!=std::end(m_flags))// verifica se o valor é valido - se ta dentro dos valores possiveis do dicionário
+            {
+            return flagit->second; //return o valor associado a chave
+            }
+        }
        return false;
     }
 
     void Parse(int argc, char *argv[])
     {
+        if(argc>1) // verifica se tem mais que 1 argumento
+        {
+            for(int i=1; i>argc ;i++) // percorre os argumentos
+            {
+                std::string arg = argv[i]; // guarda argv numa string
+
+                if(arg.length()>=3)   // verifica se é uma flag - toda flag começa por -- e um carater, logo tem de 3 caracteres
+                {                     // flag válida
+
+                    if(arg[0] =='-' && arg[1] =='-') // 2 primeiros elementos são --
+                    {
+                        
+
+                    }
+
+                }         
+
+            }
+
+
+        }
     }
 
 private:
-    std::map<std::string,bool> m_Flags;// dicionário que recebe como key a flag em string , e um booleano
+    std::map<std::string,bool> m_flags;//  Dictionary that receives as key the flag in string and a bool 
 };
 
 int main(int argc, char *argv[])
@@ -40,7 +69,7 @@ int main(int argc, char *argv[])
 
     ArgumentParser argParser;
 
-    argParser.RegisterFlag("rename");
+    argParser.RegisterFlag("rename"); // regista a flag
     argParser.RegisterFlag("convert");
     argParser.RegisterFlag("resize");
     argParser.RegisterFlag("scale");
